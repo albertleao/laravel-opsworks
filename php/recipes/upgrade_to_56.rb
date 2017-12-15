a90ca92f55d73740a1dab790d763aa27968cc11d
albertleao <albertleao@live.com>
2017-12-15 18:49:07 +0000

    upgrade to php 5.6

---
+++ php/recipes/upgrade_to_56.rb
@@ -0,0 +1,39 @@
+case node[:platform]
+when "debian", "ubuntu"
+  execute "add_repo" do
+    user "root"
+    command "add-apt-repository ppa:ondrej/php"
+    notifies :run, 'execute[apt_update]', :immediately
+  end
+
+  execute "apt_update"
+    user "root"
+    command "apt-get update"
+    notifies :run, 'execute[apt_install]', :immediately
+  end
+
+  execute "apt_package"
+    apt_package ['php5.6', 'php5.6-mbstring', 'php5.6-mcrypt', 'php5.6-mysql', 'php5.6-xml'] do
+      retries 3
+      retry_delay 5
+      notifies :run, 'execute[disable_php5_apache]', :delayed
+    end
+  end
+
+  execute "disable_php5_apache"
+    user "root"
+    command "a2dismod php5"
+    notifies :run, 'execute[enable_php5_apache]', :immediately
+  end
+
+  execute "enable_php5_apache"
+    user "root"
+    command "a2enmod php5.6"
+    notifies :run, 'execute[restart_apache]', :immediately
+  end
+
+  execute "restart_apache"
+    user "root"
+    command "service apache2 restart"
+  end
+end
